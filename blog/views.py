8102ad08112e001e67d5c34b9619fbc5f28352f0
plesniak <piotrlesniak@interia.eu>
2025-06-09 09:27:23 +0200

    Dodano widoki do bloga

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def details(request, id):
+    return render(request, 'blog/details.html', {'id':id})
